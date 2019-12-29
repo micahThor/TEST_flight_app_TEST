29b2be29cfc31ecc5612715c12bbf7cc5a1b2078
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-12-29 14:13:11 -0800

    added more api endpoint examples

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const superagent = require('superagent');
 require('dotenv').config();
 
 
-const url = `https://adsbexchange-com1.p.rapidapi.com/registration/A6-EOL/`;
+//////////////////////////////
+  ///// EXAMPLE QUERIES /////
+//////////////////////////////
+
+// API call with aircraft registration.  Good for tracking flights of a single aircraft.  We can possible save all flights of a single aircraft in our database.
+const bySingleAircraftRegistrationURL = `https://adsbexchange-com1.p.rapidapi.com/registration/A6-EOL/`;
+
+// API call with latitude and longitude and distance.  Returns all aircraft within that area's radius.
+const byLocationAndWithinRadiusURL = `https://adsbexchange-com1.p.rapidapi.com/json/lat/47.6062/lon/122.3321/dist/25/`;
+
+// API call with a squawk code.  Returns all aircraft with that active sqwuak code.
+const bySquawkURL = `https://adsbexchange-com1.p.rapidapi.com/sqk/1200/`;
 
 
-superagent.get(url)
+// use superagent to retrieve data from API endpoints
+superagent.get(bySquawkURL)
   .set('x-rapidapi-host', `adsbexchange-com1.p.rapidapi.com`)
-  .set('x-rapidapi-key', `${process.env.FLIGHT_API_KEY}`).then(dataFromEndpoint => {
+  .set('x-rapidapi-key', `${process.env.FLIGHT_API_KEY}`)
+  .then(dataFromEndpoint => {
 
     console.log(dataFromEndpoint.body);
 
